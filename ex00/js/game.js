9bf53e0f49f59b11311911badd3add6f7fc31a31
Ignacio Viseras <ignacio.viseras@gmail.com>
2025-12-01 16:19:25 +0100

    init js logic

--- ex00/js/game.js
+++ ex00/js/game.js
@@ -5,13 +5,15 @@ const game = [
     [0,0,0,0]
 ];
 
-function init() {
+function createGrid() {
     const gameTable = document.querySelector(".game-table");
     const score = document.querySelector("#score-value");
-    // const gameTable = document.querySelector(".game-table");//pillar este para el score-value
+    const scoreValue = 0;
     gameTable.innerHTML = "";
-    score.innerHTML = 0;
+    score.innerHTML = scoreValue;
     
+    game[getRandom(4)][getRandom(4)] = Math.random() < 0.5 ? 2 : 4;
+    game[getRandom(4)][getRandom(4)] = Math.random() < 0.5 ? 2 : 4;
     for (let row of game) {
         for (let value of row) {
             const cell = document.createElement("div");
@@ -21,6 +23,21 @@ function init() {
         }
         
     }
+    console.table(game);
+    // initGame();
 }
 
-init();
+/**
+    Si le pasamos 3 generara aleatorio de 0 1 2
+*/
+function getRandom(x) {
+    return Math.floor(Math.random() * x);
+
+}
+
+// function initGame() {
+    
+
+// }
+
+createGrid();
