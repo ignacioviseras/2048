d8c65a4cb5e175a5981b8663bd393b2264012b22
Ignacio Viseras <ignacio.viseras@gmail.com>
2025-12-01 17:27:01 +0100

    add uprow

--- ex00/js/game.js
+++ ex00/js/game.js
@@ -4,11 +4,11 @@ const game = [
     [0,0,0,0],
     [0,0,0,0]
 ];
+const scoreValue = 0;
 
 function createGrid() {
     const gameTable = document.querySelector(".game-table");
     const score = document.querySelector("#score-value");
-    const scoreValue = 0;
     gameTable.innerHTML = "";
     score.innerHTML = scoreValue;
     
@@ -21,10 +21,24 @@ function createGrid() {
             cell.textContent = value == 0 ? "": value;
             gameTable.appendChild(cell);
         }
-        
     }
     console.table(game);
     playGame();
+}
+
+function updateGrid(table) {
+    const gameTable = document.querySelector(".game-table");
+    const cells = gameTable.children;
+    let x = 0;
+
+    for (let i = 0; i < game.length; i++) {
+        for (let j = 0; j < game[i].length; j++) {
+            let value = game[i][j];
+            cells[x].textContent = value == 0 ? "": value;
+            x++;
+        }
+        
+    }
 }
 
 /**
@@ -34,14 +48,33 @@ function getRandom(x) {
     return Math.floor(Math.random() * x);
 }
 
+function handleUP() {
+    console.log("up");
+    for (let i = 0; i < game.length; i++) {
+        for (let j = 0; j < game[i].length; j++) {
+            let value = game[i][j];
+            let x = i
+            while (x > 0 && game[x-1][j] === 0) {
+                game[x-1][j] = game[x][j]; // mover el valor hacia arriba
+                game[x][j] = 0;            // vaciar la celda original
+                x--;
+            }
+
+            // game[x][j] = value;
+        }
+    }
+    updateGrid();
+
+    
+}
+
 function playGame() {
     document.addEventListener("keydown", function(event) {
        if (["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].includes(event.key))
         event.preventDefault();
         switch(event.key) {
             case "ArrowUp":
-                // handleUP();
-                console.log("up");
+                handleUP();
                 break;
             case "ArrowDown":
                 // handleDown();
